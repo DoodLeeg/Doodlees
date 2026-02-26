417f1144038cee69839256314c8a976c1d90ba94
Doo <tpnfrskb@example.com>
2026-02-26 02:45:59 -0800

    Okak

--- routes/web.php
+++ routes/web.php
@@ -8,26 +8,10 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return ('!!!');
 });
-// Задачи по artisan и путям контроллера
 Route::get('/name/all', [UserController::class, 'show']);
-
 Route::get('user/{name}', [OkakController::class, 'show']);
-
 Route::get('user/{Surname}/{Name}', [OtakController::class, 'show']);
-
-// Массив данных имен и городов задача A
 ?>
