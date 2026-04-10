8ee53a67df73231955459e04f882fba8a60e4319
Doo <tpnfrskb@example.com>
2026-04-10 00:31:07 -0700

    PR-7

--- routes/web.php
+++ routes/web.php
@@ -15,41 +15,7 @@ Route::get('/', function () {
 });
 // Z1
 
-Route::get('/git/{name}/{age}/{salary}', [UsersController::class, 'show'])->name('Worker.Worker');
 
-// Z2
-
-Route::get('/git/{color}',  [ColorController::class, 'css']);
-
-// Z3 
-
-Route::get('/git/cnt/input', [InputController::class,'inputt']);
-
-// Z5
-
-Route::get('/git/t/text', [TextController::class,'text']);
-
-// Z6
-
-Route::get('/git/time/data',[DataController::class,'data']);
-
-// Z7
-
-Route::get('/git/w/Rabot', [RabotController::class,'Rabot']);
-
-// Z8
-
-Route::get('/git/mas/massive', [MassController::class,'mas']);
-
-// Z9
-
-Route::get('/git/c/city', [CityController::class,'city']);
-
-// Z10
-
-Route::get('/git/s/trd', [STDAController::class, 'std']);
-
-// Z12
 
 
 ?>
