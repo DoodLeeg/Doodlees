fdee11e61b015418811ff0533cf4a8ef3dad08d3
Doo <tpnfrskb@example.com>
2026-04-23 04:26:37 -0700

    PR-7

---
+++ database/migrations/2026_04_23_102457_change_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+		Schema::table('users2', function (Blueprint $table) 
+        {
+			$table->string('email');
+			$table->integer('salary');
+            $table->integer('age');
+		});
+	}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users2', function (Blueprint $table) 
+        {
+			$table->dropColumn('email');
+			$table->dropColumn('salary');
+            $table->dropColumn('age');
+		});
+           
+    }
+};
